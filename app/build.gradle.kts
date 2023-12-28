597092e1d795f5e0086e7df58b9acc25316c3b95
amirkazemzade <amirkzm0098@gmail.com>
2023-12-28 18:55:19 +0330

    fix: problem in schema directory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.4"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
