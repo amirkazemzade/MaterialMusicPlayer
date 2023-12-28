48535f98072f4e66a37280fb02202aabaf5b7709
amirkazemzade <amirkzm0098@gmail.com>
2023-12-28 18:58:08 +0330

    version: pre-release v0.2.1
    added cache for musics list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
